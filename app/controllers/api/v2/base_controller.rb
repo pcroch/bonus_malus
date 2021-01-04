7f10b20b192740b0b2cd05b0b0fef34c521711a9
pierre crochet <pierrecrochet03@gmail.com>
2021-01-04 19:03:18 +0100

    error handeler with number

--- app/controllers/api/v2/base_controller.rb
+++ app/controllers/api/v2/base_controller.rb
@@ -1,24 +1,17 @@
 class Api::V2::BaseController < ActionController::API
-  # before_action :ensure_json_request
 
   private
-
-  # def ensure_json_request
-  #   return if request.format == :json
-  #   request.format.symbol == :json
-  #   render_errors([{ code: '1', message: 'Requests need to be in JSON format.'}], :not_acceptable)#406
-  # end
 
   def render_errors(errors = [], status = :ok)
     render json: { errors: errors }, status: status
   end
 
-  def render_errors_layout
-    render_errors([{ code: '1', message: 'Incorrect layout.' }], :not_acceptable)
+  def render_errors_missing_params
+    render_errors([{ code: '1', message: 'Missing params' }], :not_acceptable)
   end
 
-  def render_errors_missing_params
-    render_errors([{ code: '2', message: 'Missing params' }], :not_acceptable)
+  def render_errors_char_string
+    render_errors([{ code: '2', message: 'Number only.' }], :not_acceptable)
   end
 
   def render_errors_missing_value
@@ -29,7 +22,9 @@ class Api::V2::BaseController < ActionController::API
     render_errors([{ code: '4', message: 'Please contact the helpdesk if sinister greater than 2' }], :not_acceptable)
   end
 
-   def render_errors_boolean
+  def render_errors_boolean
     render_errors([{ code: '5', message: 'Pro params should be 0 or 1' }], :not_acceptable)
   end
 end
+
+
--- app/controllers/api/v2/scores_controller.rb
+++ app/controllers/api/v2/scores_controller.rb
@@ -12,13 +12,15 @@ class Api::V2::ScoresController < Api::V2::BaseController
   def error_handeler
     if @years.nil? || @sinister.nil? || @pro.nil?
       render_errors_missing_params
+    elsif @years.first.match?(/\D/) || @sinister.first.match?(/\D/) || @pro.first.match?(/\D/)
+      render_errors_char_string
     elsif @years.first.empty? || @sinister.first.empty? || @pro.first.empty?
       render_errors_missing_value
     elsif @sinister.first.to_i > 2
       render_errors_min_max
     elsif @pro.first.to_i > 1
       render_errors_boolean
-    elsif
+    else
       @sinister = @sinister.first.to_i
       @years = @years.first.to_i
       @pro = @pro.first.to_i
@@ -43,10 +45,14 @@ end
 
 # for debugging only
 #      p "hello"
+# regex = /\D/
+# p   @sinister.first.class
+# p   @sinister
 
-# p  @sinister.nil?
+# p  @sinister.first =~ /\D/
 
 #    p "bye bye"
 
 
-
+# for debugging only
+  # render json: { Bonus_Malus_Score: "computation_string", status: :success }
