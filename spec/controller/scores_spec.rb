f37d8c81bdf0623142d7520a754e38352e8866c8
pierre crochet <pierrecrochet03@gmail.com>
2021-01-18 16:44:32 +0000

    integration testing done

--- spec/controller/scores_spec.rb
+++ spec/controller/scores_spec.rb
@@ -7,14 +7,74 @@
 #   end
 # end
 require 'rails_helper'
-RSpec.describe 'Score  API', type: :request do
+RSpec.describe 'Querry string: computation', type: :request do
   # Test suite for GET /articles
-  describe 'GET /api/v2/scores' do
+  describe 'General computation should return 22' do
     # make HTTP get request before each example
     before { get 'http://localhost:3000/api/v2/scores/?sinister[]=2&years[]=2&pro[]=1' }
 
-    it 'returns a validation of the querry string' do
+    it 'With the follozing params: sinister[]=2&years[]=2&pro[]=1' do
       expect(response.body).to match("{\"Bonus_Malus_Score\":22,\"status\":\"success\"}")
+    end
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'General computation should return 18' do
+    # make HTTP get request before each example
+    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=1&years[]=1&pro[]=1' }
+
+    it 'With the follozing params: sinister[]=1&years[]=1&pro[]=1' do
+      expect(response.body).to match("{\"Bonus_Malus_Score\":18,\"status\":\"success\"}")
+    end
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+    describe 'General computation should return 12' do
+    # make HTTP get request before each example
+    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=0&years[]=2&pro[]=1' }
+
+    it 'With the follozing params: sinister[]=0&years[]=2&pro[]=1' do
+      expect(response.body).to match("{\"Bonus_Malus_Score\":12,\"status\":\"success\"}")
+    end
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+    describe 'General computation should return 2' do
+    # make HTTP get request before each example
+    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=0&years[]=12&pro[]=1' }
+
+    it 'With the follozing params: sinister[]=0&years[]=12&pro[]=1' do
+      expect(response.body).to match("{\"Bonus_Malus_Score\":2,\"status\":\"success\"}")
+    end
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'Minimum score of -2 no matter the years selected' do
+    # make HTTP get request before each example
+    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=0&years[]=2222&pro[]=1' }
+
+    it 'With the follozing params: sinister[]=0&years[]=2222&pro[]=1' do
+      expect(response.body).to match("{\"Bonus_Malus_Score\":-2,\"status\":\"success\"}")
+    end
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+    describe 'Minimum score of -2 no matter the years selected' do
+    # make HTTP get request before each example
+    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=0&years[]=23&pro[]=1' }
+
+    it 'With the follozing params: sinister[]=0&years[]=23&pro[]=1' do
+      expect(response.body).to match("{\"Bonus_Malus_Score\":-2,\"status\":\"success\"}")
     end
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
@@ -24,7 +84,7 @@ end
 
 # testing the errors codes 1
 
-RSpec.describe 'Testing the errors code 1  API', type: :request do
+RSpec.describe 'Error code 1: Missing params', type: :request do
   # Test suite for GET /articles
 
   describe 'error code one: missing pro params' do
@@ -77,9 +137,9 @@ RSpec.describe 'Testing the errors code 1  API', type: :request do
 end
 
 
-# testing the errors codes 1
+# testing the errors codes 2
 
-RSpec.describe 'Testing the errors code 2  API', type: :request do
+RSpec.describe 'Error code 2: Number only', type: :request do
   # Test suite for GET /articles
 
   describe 'error code two: Not an integer for sinister params' do
@@ -129,5 +189,94 @@ RSpec.describe 'Testing the errors code 2  API', type: :request do
       expect(response).to have_http_status(406)
     end
   end
+end
+    # testing the errors codes 3
+
+RSpec.describe 'Error code 3: Missing value(s)', type: :request do
+  # Test suite for GET /articles
+
+  describe 'Error code 3: Missing value(s) of sinister params' do
+    # make HTTP get request before each example
+    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=&years[]=1&pro[]=1' }
+
+    it 'Return validation for error code 3: Value missing in the querry string' do
+      expect(response.body).to match("{\"errors\":[{\"code\":\"3\",\"message\":\"Missing value(s)\"}]}")
+    end
+    it 'returns status code 406: Not acceptable' do
+      expect(response).to have_http_status(406)
+    end
+  end
+
+  describe 'Error code 3: Missing value(s) of years params' do
+    # make HTTP get request before each example
+    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=1&years[]=&pro[]=1' }
+
+    it 'Return validation for error code 3: Value missing in the querry string' do
+      expect(response.body).to match("{\"errors\":[{\"code\":\"3\",\"message\":\"Missing value(s)\"}]}")
+    end
+    it 'returns status code 406: Not acceptable' do
+      expect(response).to have_http_status(406)
+    end
+  end
+
+  describe 'Error code 3: Missing value(s) of pro params' do
+    # make HTTP get request before each example
+    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=1&years[]=1&pro[]=' }
+
+    it 'Return validation for error code 3: Value missing in the querry string' do
+      expect(response.body).to match("{\"errors\":[{\"code\":\"3\",\"message\":\"Missing value(s)\"}]}")
+    end
+    it 'returns status code 406: Not acceptable' do
+      expect(response).to have_http_status(406)
+    end
+  end
+
+  describe 'Error code 3: Missing all value(s)' do
+    # make HTTP get request before each example
+    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=&years[]=&pro[]=' }
+
+    it 'Return validation for error code 3: Value missing in the querry string' do
+      expect(response.body).to match("{\"errors\":[{\"code\":\"3\",\"message\":\"Missing value(s)\"}]}")
+    end
+    it 'returns status code 406: Not acceptable' do
+      expect(response).to have_http_status(406)
+    end
+  end
+end
+
+    # testing the errors codes 4
+
+RSpec.describe 'Error code 4: Sinister value greater than 2', type: :request do
+  # Test suite for GET /articles
+  describe 'Error code 4: Sinister value greater than 2' do
+    # make HTTP get request before each example
+    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=3&years[]=2&pro[]=1' }
+
+    it 'returns a validation of the querry string' do
+      expect(response.body).to match("{\"errors\":[{\"code\":\"4\",\"message\":\"Please contact the helpdesk if sinister greater than 2\"}]}")
+    end
+    it 'returns status code 406: Not acceptable' do
+      expect(response).to have_http_status(406)
+    end
+  end
+end
+
+    # testing the errors codes 4
+
+RSpec.describe 'Error code 5: Pro params should be 0 or 1', type: :request do
+  # Test suite for GET /articles
+  describe 'Error code 5: Pro params is equal to 2' do
+    # make HTTP get request before each example
+    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=1&years[]=2&pro[]=2' }
+
+    it 'returns a validation of the querry string' do
+      expect(response.body).to match("{\"errors\":[{\"code\":\"5\",\"message\":\"Pro params should be 0 or 1\"}]}")
+    end
+    it 'returns status code 406: Not acceptable' do
+      expect(response).to have_http_status(406)
+    end
+  end
+
+
 
 end
