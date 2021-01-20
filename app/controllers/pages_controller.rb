747fe5c451b39977f6aefeb851c6c381f3ca62a3
pierre crochet <pierrecrochet03@gmail.com>
2021-01-20 18:28:23 +0000

    improove title

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
 
   def home
-    render json: { Bonus_Malus_Score: "This is an api please read refer to documentation in https://github.com/pcroch/bonus_malus ", status: :error }
+    render json: { Bonus_Malus_Score: "This is an api please refer to https://github.com/pcroch/bonus_malus for the documentation", status: :error }
 
   end
 end
