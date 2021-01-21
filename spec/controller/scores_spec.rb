ffd2199264aaee725464264e701de15fd03dc2f6
pierre crochet <pierrecrochet03@gmail.com>
2021-01-21 11:10:54 +0000

    tesintg adapted

--- spec/controller/scores_spec.rb
+++ spec/controller/scores_spec.rb
@@ -2,12 +2,19 @@
 require 'rails_helper'
 require 'spec_helper'
 
+### version definition
+version = "v2"
+
+## please ;odify the object version to adapt as per
+## the api version you want to to test
+object_version = Api::V2::ScoresController.new
+
 ### Integration testing
 RSpec.describe 'Integration testing for Querry string: computation', type: :request do
   # Test suite for GET /articles
   describe 'General computation should return 22' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=2&years[]=2&pro[]=1' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=2&years[]=2&pro[]=1" }
 
     it 'With the follozing params: sinister[]=2&years[]=2&pro[]=1' do
       expect(response.body).to match("{\"Bonus_Malus_Score\":22,\"status\":\"success\"}")
@@ -19,7 +26,7 @@ RSpec.describe 'Integration testing for Querry string: computation', type: :requ
 
   describe 'General computation should return 18' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=1&years[]=1&pro[]=1' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=1&years[]=1&pro[]=1" }
 
     it 'With the follozing params: sinister[]=1&years[]=1&pro[]=1' do
       expect(response.body).to match("{\"Bonus_Malus_Score\":18,\"status\":\"success\"}")
@@ -31,7 +38,7 @@ RSpec.describe 'Integration testing for Querry string: computation', type: :requ
 
     describe 'General computation should return 12' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=0&years[]=2&pro[]=1' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=0&years[]=2&pro[]=1" }
 
     it 'With the follozing params: sinister[]=0&years[]=2&pro[]=1' do
       expect(response.body).to match("{\"Bonus_Malus_Score\":12,\"status\":\"success\"}")
@@ -43,7 +50,7 @@ RSpec.describe 'Integration testing for Querry string: computation', type: :requ
 
     describe 'General computation should return 2' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=0&years[]=12&pro[]=1' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=0&years[]=12&pro[]=1" }
 
     it 'With the follozing params: sinister[]=0&years[]=12&pro[]=1' do
       expect(response.body).to match("{\"Bonus_Malus_Score\":2,\"status\":\"success\"}")
@@ -55,7 +62,7 @@ RSpec.describe 'Integration testing for Querry string: computation', type: :requ
 
   describe 'Minimum score of -2 no matter the years selected' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=0&years[]=2222&pro[]=1' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=0&years[]=2222&pro[]=1" }
 
     it 'With the follozing params: sinister[]=0&years[]=2222&pro[]=1' do
       expect(response.body).to match("{\"Bonus_Malus_Score\":-2,\"status\":\"success\"}")
@@ -67,7 +74,7 @@ RSpec.describe 'Integration testing for Querry string: computation', type: :requ
 
     describe 'Minimum score of -2 no matter the years selected' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=0&years[]=23&pro[]=1' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=0&years[]=23&pro[]=1" }
 
     it 'With the follozing params: sinister[]=0&years[]=23&pro[]=1' do
       expect(response.body).to match("{\"Bonus_Malus_Score\":-2,\"status\":\"success\"}")
@@ -85,7 +92,7 @@ RSpec.describe 'Integration testing for Error code 1: Missing params', type: :re
 
   describe 'error code one: missing pro params' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=2&years[]=2' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=2&years[]=2" }
 
     it 'Return validation for error code 1: pro params missing' do
       expect(response.body).to match("{\"errors\":[{\"code\":\"1\",\"message\":\"Missing params\"}]}")
@@ -97,7 +104,7 @@ RSpec.describe 'Integration testing for Error code 1: Missing params', type: :re
 
   describe 'error code one: missing years params' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=2&pro[]=1' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=2&pro[]=1" }
 
     it 'Return validation for error code 1: years params missing' do
       expect(response.body).to match("{\"errors\":[{\"code\":\"1\",\"message\":\"Missing params\"}]}")
@@ -109,7 +116,7 @@ RSpec.describe 'Integration testing for Error code 1: Missing params', type: :re
 
   describe 'error code one: missing sinister params' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?years[]=2&pro[]=1' }
+    before { get "http://localhost:3000/api/#{version}/scores/?years[]=2&pro[]=1" }
 
     it 'Return validation for error code 1: sinister params missing' do
       expect(response.body).to match("{\"errors\":[{\"code\":\"1\",\"message\":\"Missing params\"}]}")
@@ -121,7 +128,7 @@ RSpec.describe 'Integration testing for Error code 1: Missing params', type: :re
 
   describe 'error code one: missing all params' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores' }
+    before { get "http://localhost:3000/api/#{version}/scores" }
 
     it 'Return validation for error code 1: sinister params missing' do
       expect(response.body).to match("{\"errors\":[{\"code\":\"1\",\"message\":\"Missing params\"}]}")
@@ -140,7 +147,7 @@ RSpec.describe 'Integration testing for Error code 2: Number only', type: :reque
 
   describe 'error code two: Not an integer for sinister params' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=Hello_World&years[]=1&pro[]=1' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=Hello_World&years[]=1&pro[]=1" }
 
     it 'Return validation for error code 2: The querry string supports only integer' do
       expect(response.body).to match("{\"errors\":[{\"code\":\"2\",\"message\":\"Number only.\"}]}")
@@ -152,7 +159,7 @@ RSpec.describe 'Integration testing for Error code 2: Number only', type: :reque
 
   describe 'error code two: Not an integer for years params' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=1&years[]=Hello_World&pro[]=1' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=1&years[]=Hello_World&pro[]=1" }
 
     it 'Return validation for error code 2: The querry string supports only integer' do
       expect(response.body).to match("{\"errors\":[{\"code\":\"2\",\"message\":\"Number only.\"}]}")
@@ -164,7 +171,7 @@ RSpec.describe 'Integration testing for Error code 2: Number only', type: :reque
 
   describe 'error code two: Not an integer for pro params' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=1&years[]=1&pro[]=Hello_World' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=1&years[]=1&pro[]=Hello_World" }
 
     it 'Return validation for error code 2: The querry string supports only integer' do
       expect(response.body).to match("{\"errors\":[{\"code\":\"2\",\"message\":\"Number only.\"}]}")
@@ -176,7 +183,7 @@ RSpec.describe 'Integration testing for Error code 2: Number only', type: :reque
 
   describe 'error code two: Not an integer for all params' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=Hello_World&years[]=Hello_World&pro[]=Hello_World' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=Hello_World&years[]=Hello_World&pro[]=Hello_World" }
 
     it 'Return validation for error code 2: The querry string supports only integer' do
       expect(response.body).to match("{\"errors\":[{\"code\":\"2\",\"message\":\"Number only.\"}]}")
@@ -193,7 +200,7 @@ RSpec.describe 'Integration testing for Error code 3: Missing value(s)', type: :
 
   describe 'Error code 3: Missing value(s) of sinister params' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=&years[]=1&pro[]=1' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=&years[]=1&pro[]=1" }
 
     it 'Return validation for error code 3: Value missing in the querry string' do
       expect(response.body).to match("{\"errors\":[{\"code\":\"3\",\"message\":\"Missing value(s)\"}]}")
@@ -205,7 +212,7 @@ RSpec.describe 'Integration testing for Error code 3: Missing value(s)', type: :
 
   describe 'Error code 3: Missing value(s) of years params' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=1&years[]=&pro[]=1' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=1&years[]=&pro[]=1" }
 
     it 'Return validation for error code 3: Value missing in the querry string' do
       expect(response.body).to match("{\"errors\":[{\"code\":\"3\",\"message\":\"Missing value(s)\"}]}")
@@ -217,7 +224,7 @@ RSpec.describe 'Integration testing for Error code 3: Missing value(s)', type: :
 
   describe 'Error code 3: Missing value(s) of pro params' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=1&years[]=1&pro[]=' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=1&years[]=1&pro[]=" }
 
     it 'Return validation for error code 3: Value missing in the querry string' do
       expect(response.body).to match("{\"errors\":[{\"code\":\"3\",\"message\":\"Missing value(s)\"}]}")
@@ -229,7 +236,7 @@ RSpec.describe 'Integration testing for Error code 3: Missing value(s)', type: :
 
   describe 'Error code 3: Missing all value(s)' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=&years[]=&pro[]=' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=&years[]=&pro[]=" }
 
     it 'Return validation for error code 3: Value missing in the querry string' do
       expect(response.body).to match("{\"errors\":[{\"code\":\"3\",\"message\":\"Missing value(s)\"}]}")
@@ -246,7 +253,7 @@ RSpec.describe 'Integration testing for Error code 4: Sinister value greater tha
   # Test suite for GET /articles
   describe 'Error code 4: Sinister value greater than 2' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=3&years[]=2&pro[]=1' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=3&years[]=2&pro[]=1" }
 
     it 'returns a validation of the querry string' do
       expect(response.body).to match("{\"errors\":[{\"code\":\"4\",\"message\":\"Please contact the helpdesk if sinister greater than 2\"}]}")
@@ -263,7 +270,7 @@ RSpec.describe 'Integration testing for Error code 5: Pro params should be 0 or 
   # Test suite for GET /articles
   describe 'Error code 5: Pro params is equal to 2' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=1&years[]=2&pro[]=2' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=1&years[]=2&pro[]=2" }
 
     it 'returns a validation of the querry string' do
       expect(response.body).to match("{\"errors\":[{\"code\":\"5\",\"message\":\"Pro params should be 0 or 1\"}]}")
@@ -274,14 +281,14 @@ RSpec.describe 'Integration testing for Error code 5: Pro params should be 0 or 
   end
 end
 
-### Integration testing only for v2 version
+### Integration testing only for #{version} version
 
 # testing querry string
 RSpec.describe 'Uit Testing the key presence and correct value for each key in the querry string', type: :request do
   # Test suite for GET /articles
   describe 'Testing the sinister key' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=2&years[]=2&pro[]=1' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=2&years[]=2&pro[]=1" }
 
     it "does it include the sinister key" do
       expect(request.params[:sinister]).to be_truthy
@@ -293,7 +300,7 @@ RSpec.describe 'Uit Testing the key presence and correct value for each key in t
 
   describe 'Testing the years key' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=2&years[]=2&pro[]=1' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=2&years[]=2&pro[]=1" }
 
     it "does it include the years key?" do
       expect(request.params[:years]).to be_truthy
@@ -305,7 +312,7 @@ RSpec.describe 'Uit Testing the key presence and correct value for each key in t
 
   describe 'includes the pro key"' do
     # make HTTP get request before each example
-    before { get 'http://localhost:3000/api/v2/scores/?sinister[]=2&years[]=2&pro[]=1' }
+    before { get "http://localhost:3000/api/#{version}/scores/?sinister[]=2&years[]=2&pro[]=1" }
 
     it "does it include the pro key?" do
       expect(request.params[:pro]).to be_truthy
@@ -322,21 +329,21 @@ RSpec.describe 'Unit Testing for the private methods' do
   # Test suite for GET /articles
   describe 'Testing error handeler method' do
     it "should raise an error" do
-      testing_object = Api::V2::ScoresController.new
+      testing_object = object_version
       expect { testing_object.error_handeler }.to raise_error(NoMethodError, /private/)
     end
   end
 
   describe 'Testing professional method' do
     it "should raise an error" do
-      testing_object = Api::V2::ScoresController.new
+      testing_object = object_version
       expect { testing_object.professional }.to raise_error(NoMethodError, /private/)
     end
   end
 
   describe 'Testing computation method' do
     it "should raise an error" do
-      testing_object = Api::V2::ScoresController.new
+      testing_object = object_version
       expect { testing_object.computation }.to raise_error(NoMethodError, /private/)
     end
   end
